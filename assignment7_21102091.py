82356a2c32b1162d4ce2640adab425f5a523700d
TUSHARTUSHAR23 <107211946+TUSHARTUSHAR23@users.noreply.github.com>
2022-07-17 00:10:33 +0530

    Add files via upload

---
+++ assignment7_21102091.py
@@ -0,0 +1,21 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:\Users\Manav Goyal\Downloads\assignment 7.py ===========
+Q4
+Enter a list of comma separated marks in [] brackets:[25,45,85,78]
+Sorted array is: 
+25 45 78 85 
+Q5
+Enter a list in [] brackets:[85,26,45,100]
+[26, 45, 85, 100]
+Enter the element you want to search :45
+Element is present at index 1
+Number of occurences of element 45 is: 1
+Q6
+Enter a list of numbers containing duplicates inside []:[12,25,78,95,12]
+[12, 25, 78, 95]
+Sorted List in Ascending Order by Bubble sort:
+[12, 25, 78, 95]
+Sorted Array in Ascending Order by Selection sort:
+[12, 25, 78, 95]
